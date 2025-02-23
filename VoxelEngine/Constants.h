e902a3c979bb054707e58571abffb385e67c1f58
Finnduas <88577279+Finnduas@users.noreply.github.com>
2025-02-23 14:18:56 +0100

    Added windowing using SDL3 part 2
    One commit would have been to large.

---
+++ VoxelEngine/Constants.h
@@ -0,0 +1,9 @@
+#pragma once
+#define TRUE 1
+#define FALSE 0
+
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
+
+#define FPS 165
+#define TARGET_FRAME_TIME (1000/FPS)
--- VoxelEngine/Source.c
+++ VoxelEngine/Source.c
@@ -1,7 +1,67 @@
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <stdio.h>
 #include <stdint.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include "Constants.h"
 
-int main() {
-	printf("Hello World! \n");
-	return 0;
+
+static SDL_Window* window = NULL;
+static SDL_GLContext glcontext;
+//static SDL_Renderer* renderer = NULL;
+
+void pre_draw() {
+    /*glClearColor(0, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT);*/
+}
+
+void draw() {
+    //TODO:
+}
+
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4); 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1); //OPENGL version 4.1
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    //probably useless
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 5);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
+
+    if (!SDL_CreateWindow("test", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE)) {
+        SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    glcontext = SDL_GL_CreateContext(window);
+    //render stuff
+    pre_draw();
+    draw();
+    //and draw it to the screen
+    SDL_GL_SwapWindow(window);
+
+    return SDL_APP_CONTINUE;
+}
+
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
+    if (event->type == SDL_EVENT_KEY_DOWN ||
+        event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+    }
+    return SDL_APP_CONTINUE;
+}
+
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void* appstate) {
+    return SDL_APP_CONTINUE;
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+    SDL_DestroyWindow(window);
+    SDL_GL_DestroyContext(glcontext);
 }
